10d7de959dd72cbbb9ba8730a626406a771f9b56
Aidaar <aidar21mamatov@gmail.com>
2023-01-16 15:41:25 +0600

    add mobile file icons

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ function App() {
 	return (
 		<div className='App'>
 			<Header />
-			{/* <About />
-			<Services />
+			<About />
+			{/* <Services />
 			<LinkBlock />
 			<Portfolio />
 			<Consultation />
