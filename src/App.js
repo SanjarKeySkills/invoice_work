f8594247cae5e94dd5264b21ab9ff4a0770319fe
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2023-01-26 17:45:05 +0600

    changes Hero (not ready)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import './App.css'
+import "./App.css";
 
-
-import About from './components/About/About'
-import Consultation from './components/Consultation/Consultation'
-import Footer from './components/Footer/Footer'
-import Header from './components/Header/Header'
-import LinkBlock from './components/LinkBlock/LinkBlock'
-import Portfolio from './components/Portfolio/Portfolio'
-import Services from './components/Services/Services'
+import About from "./components/About/About";
+import Consultation from "./components/Consultation/Consultation";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import LinkBlock from "./components/LinkBlock/LinkBlock";
+import Portfolio from "./components/Portfolio/Portfolio";
+import Services from "./components/Services/Services";
 
 function App() {
-	return (
-		<div className='App'>
-			<Header />
-			{/* <About /> */}
-			{/* <Services /> */}
-			{/* <LinkBlock /> */}
-			{/* <Portfolio /> */}
-			<Consultation />
-			{/* <Footer /> */}
-		</div>
-	)
+    return (
+        <div className="App">
+            <Header />
+            <About />
+            <Services />
+            {/* <LinkBlock /> */}
+            {/* <Portfolio /> */}
+            {/* <Consultation /> */}
+            {/* <Footer /> */}
+        </div>
+    );
 }
 
-export default App
+export default App;
